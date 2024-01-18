e0934936848eaef77f9f34cbeae2cd490db25035
Daniel Hang <daniel.jjun5@gmail.com>
2024-01-18 14:22:05 +0800

    - First Init

---
+++ src/test/java/com/dacidevspring/serviceregistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dacidevspring.serviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
